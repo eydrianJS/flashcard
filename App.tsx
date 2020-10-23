7deeab31e4df6d55d2e5a1755620ac8cd522bd9e
Adrian Olszowski <aol@spyro-soft.com>
2020-10-24 00:18:45 +0200

    start creating app

--- App.tsx
+++ App.tsx
@@ -1,21 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react';
+import { TextInput, View, Button, StyleSheet, Text } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [goals, onChangeGoals] = useState<string>('');
+  const [currentGoals, setCurrentGoals] = useState<string[]>([]);
+
+  const handleChangeGoals = useCallback((text) => onChangeGoals(text), [goals]);
+  const handleAddGoals = useCallback(() => {
+    setCurrentGoals((currentGoals) => [...currentGoals, goals]);
+    handleChangeGoals('');
+  }, [currentGoals, goals]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputCointer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={handleChangeGoals}
+          placeholder="Your Goals"
+          value={goals}
+        />
+        <Button title="ADD" onPress={handleAddGoals} />
+      </View>
+      <View>
+        {currentGoals.map((goal, idx) => (
+          <Text key={idx}>{goal}</Text>
+        ))}
+      </View>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
+  inputCointer: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
+  input: { height: 40, borderColor: 'black', borderWidth: 1, padding: 10, width: '80%' },
 });
